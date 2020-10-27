0cea91feb8301176ea1f01015f9c1d01818923e4
wangweijuan <958918764@qq.com>
2020-10-27 22:42:06 +0800

    完成 头部header样式布局

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
 import './index.less'
 
@@ -5,7 +7,18 @@ export default class Header extends Component {
     render() {
         return (
             <div className='header'>
-                header
+                <div className='header-top'>
+                    <span>欢迎，admin</span>
+                    <a href='javascript:(0)'>退出</a>
+                </div>
+                <div className='header-bottom'>
+                    <div className='header-bottom-left'>首页</div>
+                    <div className='header-bottom-right'>
+                        <span>2020-10-27 22:17</span>
+                        <img src='http://api.map.baidu.com/images/weather/day/qing.png' alt='weather'/>
+                        <span>晴</span>
+                    </div>
+                </div>
             </div>
         )
     }
